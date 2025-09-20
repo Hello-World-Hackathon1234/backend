d108f79dea1ce6074dfd7921d199acf87fc9cb64
JohnSwiftC <johnswift@live.com>
2025-09-20 12:34:25 -0400

    Pydantic modelling for clubs, basic database testing

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from typing import List
+import schema
+from database import get_db
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.get("/clubs", response_model=List[schema.ClubModel])
+async def get_club_list(db: Session = Depends(get_db)):
+    clubs = db.query(schema.Club)
+
+    return clubs
--- schema.py
+++ schema.py
@@ -1,11 +1,14 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean, text
+from pydantic import BaseModel
 
 class User(Base):
     __tablename__ = "Users"
 
     id = Column(Integer,primary_key=True,nullable=False)
     name = Column(String,nullable=False)
+
+# Implement this later for users
 
 class Club(Base):
     __tablename__ = "Clubs"
@@ -15,4 +18,15 @@ class Club(Base):
     description = Column(String,nullable=False)
     tags = Column(String,nullable=True)
     imageUrl = Column(String,nullable=True)
+
+class ClubModel(BaseModel):
+
+    id: int
+    name: str
+    description: str
+    tags: str
+    imageUrl: str
+    
+    class Config:
+        orm_model = True
 
